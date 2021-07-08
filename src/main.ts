2d47ad92c4be2fc702aaabdbd368a6e11daa0230
Brian K. Kiragu <brian@amprest.co.ke>
2021-07-08 17:31:13 +0300

    Changed gitignore

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ import BusinessList from './js/components/BusinessList.vue';
 // Create the Vue instance.
 const app = createApp({
   components: { BusinessList },
+  data() {
+    return {
+      people: [
+        { id: 32123424, name: 'James Veitch', age: 28 },
+        { id: 62164364, name: 'Libro Manhaam', age: 32 },
+      ]
+    }
+  }
 });
 
 // Mount the application.
